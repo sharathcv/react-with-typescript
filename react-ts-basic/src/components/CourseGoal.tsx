bb185afdb9564295ec3c9294e511fd8fc217879d
sharathcv <sharathcv@gmail.com>
2025-04-19 00:30:33 +1000

    Basic App and CourseGoal component check-in

---
+++ react-ts-basic/src/components/CourseGoal.tsx
@@ -0,0 +1,12 @@
+
+export default function CourseGoal(props) {
+
+    return <article>
+            <div>
+                <h2>Title</h2>
+                <p>Description</p>
+            </div>   
+            <button>Delete</button>         
+        </article>
+
+}
