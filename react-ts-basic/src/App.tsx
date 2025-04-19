3e1e72a1493a0e930c847a2dfa809b468824d988
sharathcv <sharathcv@gmail.com>
2025-04-19 13:51:37 +1000

    CourseGoal handler added

--- react-ts-basic/src/App.tsx
+++ react-ts-basic/src/App.tsx
@@ -1,16 +1,44 @@
 import CourseGoal from "./components/CourseGoal"; 
 import goalsImg from './assets/goals.jpg';
 import Header from "./components/Header";
+import {useState} from "react";
+
+type CourseGoalData = {
+  title: string;
+  description: string;
+  id: number;
+};
 
 export default function App() {
+  const [goals, setGoals] = useState<CourseGoalData[]>([]);
+
+  function handleAddGoal() {
+    setGoals(prevGoals => {
+      const newGoal: CourseGoalData = {
+        id: Math.random(),
+        title: "Learn",
+        description: "Learn in depth"
+      };
+
+      return [...prevGoals, newGoal];
+    });
+  }
+
   return (
     <main>
       <Header image={{ src: goalsImg, alt: "A list of goals"}}>
         <h1>Your Course Goals</h1>
       </Header>
-      <CourseGoal title="Learn React + TS">
-        <p>Learn it from the ground up.</p>
-      </CourseGoal>
+      <button onClick={handleAddGoal}>Add Goal</button>
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal title={goal.title}>
+              <p>{goal.description}</p>
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 }
