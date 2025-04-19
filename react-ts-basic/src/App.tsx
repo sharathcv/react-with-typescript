bfd5df48fed52797460b0e0842adc07d406f213b
sharathcv <sharathcv@gmail.com>
2025-04-19 13:24:33 +1000

    Header component added along with index.d.ts to typify jpg file

--- react-ts-basic/src/App.tsx
+++ react-ts-basic/src/App.tsx
@@ -1,9 +1,15 @@
 import CourseGoal from "./components/CourseGoal"; 
+import goalsImg from './assets/goals.jpg';
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <main>
-      <CourseGoal title="Learn React + TS" description="Learn it from the ground up">
+      <Header image={{ src: goalsImg, alt: "A list of goals"}}>
+        <h1>Your Course Goals</h1>
+      </Header>
+      <CourseGoal title="Learn React + TS">
+        <p>Learn it from the ground up.</p>
       </CourseGoal>
     </main>
   );
--- react-ts-basic/src/components/CourseGoal.tsx
+++ react-ts-basic/src/components/CourseGoal.tsx
@@ -1,17 +1,17 @@
+import { ReactNode } from "react";
+
+interface CourseGoalProps { // we can make it a type as well like type CourseGoalProps = { // fields }
+    title: string;
+    children: ReactNode;
+};
 
 //export default function CourseGoal(props: ) { // instead of props we use destructuring as shown below
-export default function CourseGoal({
-    title,
-    description
-}: {
-    title: string;
-    description: string
-}) { // instead of props we use destructuring as shown below
+export default function CourseGoal({title, children}: CourseGoalProps) { // instead of props we use destructuring as shown below
 
     return <article>
             <div>
                 <h2>{title}</h2>
-                <p>{description}</p>
+                {children}
             </div>   
             <button>Delete</button>         
         </article>
