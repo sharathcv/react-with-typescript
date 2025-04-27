7b02a17fb34a6ff3802d9095a21edb3e414e3cc8
sharathcv <sharathcv@gmail.com>
2025-04-27 21:41:22 +1000

    Button wrapper component

--- adv-components/src/App.tsx
+++ adv-components/src/App.tsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Input from "./components/Input";
+import Button from "./components/Button";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,6 +12,8 @@ function App() {
     <main>
       <Input id="name" label="Your name" type="text"/>
       <Input id="age" label="Your age" type="number" />
+      <Button el="button">A Button</Button>
+      <Button el="anchor" href="https://www/sharathcv.com">A Link</Button>
     </main>
   );
 }
