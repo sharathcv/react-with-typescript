31abd1be58accf1da21cf3af5576e75f86d64804
sharathcv <sharathcv@gmail.com>
2025-04-29 21:23:16 +1000

    AddTimer basic component created and App updated

--- adv-components/src/App.tsx
+++ adv-components/src/App.tsx
@@ -6,36 +6,44 @@ import Input from "./components/UI/Input";
 import Button from "./components/UI/Button";
 import Container from "./components/UI/Container";
 import Form, {FormHandle} from "./components/UI/Form";
+import AddTimer from './components/AddTimer';
+import Header from './components/Header';
+import Timers from './components/Timers';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const input = useRef<HTMLInputElement>(null);
-  const customForm = useRef(null);
+  // const [count, setCount] = useState(0);
+  // const input = useRef<HTMLInputElement>(null);
+  // const customForm = useRef(null);
 
-  function handleSave(data: unknown) {
-    const extractedData = data as {name: string, age: string;};
-    customForm.current.clear();
-  }
+  // function handleSave(data: unknown) {
+  //   const extractedData = data as {name: string, age: string;};
+  //   customForm.current.clear();
+  // }
 
   return (
-    <main>
-      <Form onSave={handleSave} ref={customForm}>
-        <Input id="name" label="Name" type="text"/>
-        <Input id="age" label="Age" type="number" />
-        <p>
-          <Button>Save</Button>
-        </p>
-      </Form>
+    <>
+      <Header />
+      <main>
+        <AddTimer />
+        <Timers />
+        {/* <Form onSave={handleSave} ref={customForm}>
+          <Input id="name" label="Name" type="text"/>
+          <Input id="age" label="Age" type="number" />
+          <p>
+            <Button>Save</Button>
+          </p>
+        </Form> */}
 
-      {/*<Input id="name" label="Your name" type="text"/>
-      <Input id="age" label="Your age" type="number" />
-      <Button>A Button</Button>
-      <Button href="https://www/sharathcv.com">A Link</Button>
-      <Container as={Button}>Click me!</Container>
-      <Input label="Test" id="test" ref={input} />
-      */}
+        {/*<Input id="name" label="Your name" type="text"/>
+        <Input id="age" label="Your age" type="number" />
+        <Button>A Button</Button>
+        <Button href="https://www/sharathcv.com">A Link</Button>
+        <Container as={Button}>Click me!</Container>
+        <Input label="Test" id="test" ref={input} />
+        */}
 
-    </main>
+      </main>
+    </>
   );
 }
 
