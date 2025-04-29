5b5de3176876139c892cacfd9510dae49a90f9eb
sharathcv <sharathcv@gmail.com>
2025-04-29 20:43:38 +1000

    Added forwardRef to the form to use in App component

--- adv-components/src/App.tsx
+++ adv-components/src/App.tsx
@@ -5,19 +5,21 @@ import './App.css'
 import Input from "./components/Input";
 import Button from "./components/Button";
 import Container from "./components/Container";
-import Form from "./components/Form";
+import Form, {FormHandle} from "./components/Form";
 
 function App() {
   const [count, setCount] = useState(0);
   const input = useRef<HTMLInputElement>(null);
+  const customForm = useRef(null);
 
   function handleSave(data: unknown) {
     const extractedData = data as {name: string, age: string;};
+    customForm.current.clear();
   }
 
   return (
     <main>
-      <Form onSave={handleSave}>
+      <Form onSave={handleSave} ref={customForm}>
         <Input id="name" label="Name" type="text"/>
         <Input id="age" label="Age" type="number" />
         <p>
--- adv-components/src/components/Form.tsx
+++ adv-components/src/components/Form.tsx
@@ -1,21 +1,40 @@
-import { FormEvent, ComponentPropsWithoutRef } from "react";
+import { forwardRef, useImperativeHandle, useRef, FormEvent, ComponentPropsWithoutRef } from "react";
 
 type FormProps = ComponentPropsWithoutRef<"form"> & {
     onSave: (value: unknown) => void;
 };
 
-export default function Form({onSave, children, ...otherProps}: FormProps) {
+export type FormHandle = {
+    clear: () => void;
+}
+
+const Form = forwardRef<FormHandle, FormProps>(function Form({onSave, children, ...otherProps},
+    ref) {
+    const form = useRef<HTMLFormElement>(null);
+
+    useImperativeHandle(ref, () => {
+        return {
+            clear(){
+                form.current.reset();
+                console.log("form Cleared");
+            }
+        }
+    });
+
     function handleSubmit(event: FormEvent<HTMLFormElement>) {
         event.preventDefault();
 
         const formData = new FormData(event.currentTarget);
         const data = Object.fromEntries(formData);
         onSave(data);
+        // form.current.reset(); // this function has been delegated to the app level
     }
 
     return (
-        <form onSubmit={handleSubmit} {...otherProps}>
+        <form onSubmit={handleSubmit} {...otherProps} ref={form}>
             {children}
         </form>
     );
-}
+});
+
+export default Form;
