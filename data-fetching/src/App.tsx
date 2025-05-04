42c27505cd0288d289419da585877d097f57db41
sharathcv <sharathcv@gmail.com>
2025-05-05 00:04:44 +1000

    Added simple error handling

--- data-fetching/src/App.tsx
+++ data-fetching/src/App.tsx
@@ -2,6 +2,7 @@ import { ReactNode, useEffect, useState } from "react";
 import { get } from "./util/http";
 import BlogPosts, { BlogPost } from "./components/BlogPosts";
 import fetchingImg from "./assets/data-fetching.png";
+import ErrorMessage from "./components/ErrorMessage";
 
 type RawDataBlogPost = {
   id: number;
@@ -12,28 +13,49 @@ type RawDataBlogPost = {
 
 function App() {
   const [fetchedPosts, setFetchedPosts] = useState<BlogPost[]>();
+  const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState<string>();
   
   useEffect(() => {
     async function fetchPosts() {
-      const data = (await get("https://jsonplaceholder.typicode.com/posts")) as RawDataBlogPost[];
+      setIsFetching(true);
 
-      const blogPosts: BlogPost[] = data.map(rawPost => {
-        return {
-          id: rawPost.id,
-          title: rawPost.title,
-          text: rawPost.body
-        };       
-      });
+      try {
+        const data = (await get("https://jsonplaceholder.typicode.com/posts")) as RawDataBlogPost[];
 
-      setFetchedPosts(blogPosts);
+        const blogPosts: BlogPost[] = data.map(rawPost => {
+          return {
+            id: rawPost.id,
+            title: rawPost.title,
+            text: rawPost.body
+          };       
+        });
+        setFetchedPosts(blogPosts);
+      }
+      catch (error) {
+        if (error instanceof Error) {
+          setError(error.message);
+        }
+      }
+
+      setIsFetching(false);
     }
 
     fetchPosts();
   }, []);
 
   let content: ReactNode;
+
+  if (error) {
+    content = <ErrorMessage text={error}></ErrorMessage>
+  }
+
   if (fetchedPosts) {
     content = <BlogPosts posts={fetchedPosts}></BlogPosts>;
+  }
+
+  if (isFetching) {
+    content = <p id="loading-fallback">Fetching posts...</p>
   }
 
   return <main>
