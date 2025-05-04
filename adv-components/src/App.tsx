7703486dd491db70ef2b0a37fd842d28a72b3b02
sharathcv <sharathcv@gmail.com>
2025-05-04 21:53:13 +1000

    Cleaned up App component

--- adv-components/src/App.tsx
+++ adv-components/src/App.tsx
@@ -1,11 +1,4 @@
-import { useState, useRef } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-// import Input from "./components/UI/Input";
-// import Button from "./components/UI/Button";
-// import Container from "./components/UI/Container";
-import Form, {FormHandle} from "./components/UI/Form";
 import AddTimer from './components/AddTimer';
 import Header from './components/Header';
 import Timers from './components/Timers';
@@ -27,22 +20,6 @@ function App() {
       <main>
         <AddTimer />
         <Timers />
-        {/* <Form onSave={handleSave} ref={customForm}>
-          <Input id="name" label="Name" type="text"/>
-          <Input id="age" label="Age" type="number" />
-          <p>
-            <Button>Save</Button>
-          </p>
-        </Form> */}
-
-        {/*<Input id="name" label="Your name" type="text"/>
-        <Input id="age" label="Your age" type="number" />
-        <Button>A Button</Button>
-        <Button href="https://www/sharathcv.com">A Link</Button>
-        <Container as={Button}>Click me!</Container>
-        <Input label="Test" id="test" ref={input} />
-        */}
-
       </main>
     </TimersContextProvider>
   );
