d6c103e65a7d5b0d3f5459b887d5d62d9121ff30
sharathcv <sharathcv@gmail.com>
2025-05-05 14:09:58 +1000

    Completed redux support for the app!

--- redux/src/components/Product.tsx
+++ redux/src/components/Product.tsx
@@ -1,3 +1,6 @@
+import { addToCart } from "../store/cart-slice";
+import { useCartDispath } from "../store/hooks";
+
 type ProductProps = {
   id: string;
   image: string;
@@ -7,12 +10,17 @@ type ProductProps = {
 };
 
 export default function Product({
+  id,
   image,
   title,
   price,
   description,
 }: ProductProps) {
-  function handleAddToCart() {}
+  const dispatch = useCartDispath();
+
+  function handleAddToCart() {
+    dispatch(addToCart({id, title, price}));
+  }
 
   return (
     <article className="product">
